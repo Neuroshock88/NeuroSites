27664533daa829ba05274224bf5827cf9c2b82c0
Justin Averette <javerette@live.com>
2020-06-27 18:23:56 -0500

    Updates to siate and created youtube module

--- Content/js/general.js
+++ Content/js/general.js
@@ -2,3 +2,8 @@
 if (window.location.href.includes("/Default/Index")) {
     window.location.href = "/";
 }
+setTimeout(function () {
+    if (document.querySelector(".twoseven-ext-tab-media-modal") != null) {
+        document.querySelector(".twoseven-ext-tab-media-modal").parentElement.parentElement.removeChild(document.querySelector(".twoseven-ext-tab-media-modal").parentElement);
+    }
+}, 50);
