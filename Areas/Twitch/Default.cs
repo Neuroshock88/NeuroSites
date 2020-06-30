608e2cc4f3998dd6afbf3efb97eddf90ef90d5f1
Justin Averette <javerette@live.com>
2020-06-29 22:15:15 -0500

    Added Twitch Module

---
+++ Areas/Twitch/Default.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeuroSites.Areas.Twitch
+{
+    public class Default
+    {
+        public PropertiesModel Properties = new PropertiesModel();
+        Default()
+        {
+            SetSettings();
+        }
+        private void SetSettings()
+        {
+            Properties.TwitchURL = "https://www.twitch.tv/neuroshock88";
+        }
+    }
+}
---
+++ Areas/Twitch/PropertiesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeuroSites.Areas.Twitch
+{
+    public class PropertiesModel
+    {
+        /// <summary>
+
+        /// <summary>
+        /// sets the Merch website URL
+        /// </summary>
+        public string TwitchURL { get; set; }
+    }
+}
